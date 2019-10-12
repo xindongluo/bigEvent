4fde98ac8494856dbb270e3a532299a69319dde4
罗新东 <2735568506@qq.com>
2019-10-12 12:53:40 +0800

    后台保存

---
+++ web_back/tool/config.js
@@ -0,0 +1,8 @@
+// config - 中文含义为配置
+// - 我们可以将项目中使用的接口地址统一保存在这个文件中
+
+var baseUrl = 'http://localhost:8000';
+
+var USER_LOGIN = baseUrl + '/admin/login';
+var USER_LOGOUT = baseUrl + '/admin/logout';
+var USER_INFO = baseUrl + '/admin/login';
